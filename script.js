54b85072e14769671335f7f1f8de4c8750419d1f
Xavier2104 <132233667+Xavier2104@users.noreply.github.com>
2023-05-01 10:35:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// Play win sound after 3 seconds
+const winSound = document.querySelector('#win-sound');
+setTimeout(() => {
+  winSound.play();
+}, spinTime);
+
+// Update UI after 3.5 seconds
+setTimeout(() => {
+  spinning = false;
+  spinBtn.disabled = false;
+  alert(`You got ${labels[stopIndex].textContent}!`);
+}, spinTime + 500);
 
